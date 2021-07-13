e19f3f94bcdc2d2dd6e22ae88c5a385962f57a2b
GaabrieleGioetto <gabriele.gioetto@gmail.com>
2021-07-13 08:39:46 +0000

    updated pca

--- outputs/lastampa/get_word_embeddings.py
+++ outputs/lastampa/get_word_embeddings.py
@@ -4,6 +4,8 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import seaborn as sns
+
 
 idTopics = []
 with open("risutati.txt") as f:
@@ -15,12 +17,15 @@ with open("risutati.txt") as f:
 word_inglesi = ["atp","dax","fed","wta","pmi","bund", "pogba","belen","jorge","inps","dani"]
 
 words = []
+word_topics = []
+
 with open("topics.txt") as f:
     for i, line in enumerate(f):
         if(i in idTopics):
             for word in line.split(" ")[:10]:
                 if word not in word_inglesi:
                     words.append(word)
+                    word_topics.append(i)
             
 fasttext.util.download_model('it', if_exists='ignore')  # Italian
 print("DOWNLOADED")
@@ -60,20 +65,36 @@ print(f"X shape 1: {len(X[0])}")
 # print("X")
 # print(X)
 
+sns.palplot(sns.color_palette("muted"))
+
+# Get Unique continents
+color_labels = list(set(word_topics))
+
+# List of colors in the color palettes
+rgb_values = sns.color_palette("Set2", len(color_labels))
+
+# Map continents to the colors
+color_map = dict(zip(color_labels, rgb_values))
+
+
+
 pca = PCA(n_components=2)
 result = pca.fit_transform(X)
 # create a scatter plot of the projection
-plt.scatter(result[:, 0], result[:, 1])
 
 with open("result_PCA.txt","w") as f:
     for i, word in enumerate(words):
-        f.write(f"{word},{result[i][0]},{result[i][1]}\n")
+        f.write(f"{word_topics[i]},{word},{result[i][0]},{result[i][1]}\n")
+
+
+print(list(map(lambda x: color_map[x], word_topics)))
+plt.scatter(result[:, 0], result[:, 1], c=list(map(lambda x: color_map[x], word_topics)))
     
     # result_np = np.asarray(result)
     # np.savetxt('result_PCA.txt', result_np, delimiter=',')
 
 fig, ax = plt.subplots()
-for i, word in enumerate(words):
-	ax.annotate(word, xy=(result[i, 0], result[i, 1]))
+# for i, word in enumerate(words):
+# 	ax.annotate(word, xy=(result[i, 0], result[i, 1]))
 
 plt.savefig('PCA.png', bbox_inches="tight")
